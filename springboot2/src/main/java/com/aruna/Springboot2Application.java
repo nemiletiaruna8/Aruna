d42303ef0af296d9fc9d3e16dc92839864013c01
HP <HP@DESKTOP-N2TMJ1N>
2023-02-20 16:22:22 +0530

    spring boot example

---
+++ springboot2/src/main/java/com/aruna/Springboot2Application.java
@@ -0,0 +1,14 @@
+package com.aruna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2Application.class, args);
+		System.out.println("success");
+	}
+
+}
